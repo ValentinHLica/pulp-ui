d76f14de87ea27029f14edff2ef1183e374717e0
bashvtini <bashvtini@gmail.com>
2020-04-14 17:38:33 +0200

    Updates

---
+++ precache-manifest.aa1862b581bb00e0582eec9014c2c4e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e394c10b5bb7eecbcf67f4f09207c5",
+    "url": "/pulp-ui/index.html"
+  },
+  {
+    "revision": "fc2dfbbe46247733346a",
+    "url": "/pulp-ui/static/css/main.903b7de4.chunk.css"
+  },
+  {
+    "revision": "8b38dbc57f326aa60955",
+    "url": "/pulp-ui/static/js/2.43217135.chunk.js"
+  },
+  {
+    "revision": "b5a9f0302cb1f13b821c56bbb8320f46",
+    "url": "/pulp-ui/static/js/2.43217135.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc2dfbbe46247733346a",
+    "url": "/pulp-ui/static/js/main.fb472019.chunk.js"
+  },
+  {
+    "revision": "0127c23715754238fd79",
+    "url": "/pulp-ui/static/js/runtime-main.08557e16.js"
+  },
+  {
+    "revision": "ac4f68b94044e72b0146b4c0d6d86648",
+    "url": "/pulp-ui/static/media/notFound.ac4f68b9.gif"
+  }
+]);
